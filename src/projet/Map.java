4aea14845619e9888a329e0a24b6b95da0647f73
Forray_Gabriel <gabriel.forray@gmail.com>
2021-11-26 09:50:49 +0100

    Added drag and drop

--- src/projet/Map.java
+++ src/projet/Map.java
@@ -9,43 +9,63 @@ import java.util.List;
 
 public class Map implements Runnable {
 
+	// The list of words that have to be processed
 	private List<String> words_list;
+
+	// The reducer thread
 	private Reduce reduce;
-	private int num;
+
+	// The number identifying the thread
+	private int id;
+
+	// The Hashmap containing the result
 	private HashMap<String, Integer> mapping;
 
-	public Map(List<String> words_list, Reduce reduce, int num) {
+	/**
+	 * Constructor for a map
+	 * 
+	 * @param words_list List of words to be processed
+	 * @param reduce     The reducer to send the result to
+	 * @param num        The id of the thread
+	 */
+	public Map(List<String> words_list, Reduce reduce, int id) {
 		super();
 		this.words_list = words_list;
 		this.reduce = reduce;
-		this.num = num;
+		this.id = id;
 		this.mapping = new HashMap<String, Integer>();
+	}
+
+	/**
+	 * Launch the thread, process the data
+	 */
+	public void run() {
+
+		// Get the logger and log the beggining of the thread
+		Logger log = Logger.getInstance();
+		log.write(this.id + " - I'm runnning");
+
+		/**
+		 * For each word from the list : If the word is in the Hashmap, add it and set
+		 * its value to 1 occurrence Else, get its old value and increase it by 1
+		 */
+		for (String word : words_list) {
+			if (mapping.get(word) == null) {
+				mapping.put(word, 1);
+			} else {
+				mapping.put(word, mapping.get(word) + 1);
+			}
+		}
+
+		//log.write(this.id + " - " + mapping);
+
+		// Send this thread's map to the reducer
+		reduce.addMap(this);
+
 	}
 
 	public HashMap<String, Integer> getMapping() {
 		return mapping;
-	}
-
-	public void run() {
-		
-		Logger log = Logger.getInstance();
-		
-		log.write("Salut je run");
-
-		for (String word : words_list) {
-			//log.write(num + " - " + word);
-			if (mapping.get(word) == null) {
-				mapping.put(word, 1);
-			}
-			else {
-				mapping.put(word, mapping.get(word)+1);
-			}
-		}
-		
-		log.write(num + " - " + mapping);
-		
-		reduce.addMap(this);
-
 	}
 
 }
--- src/projet/Reduce.java
+++ src/projet/Reduce.java
@@ -8,92 +8,143 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
 
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 public class Reduce implements Runnable {
-	
+
+	/**
+	 * The final map with the result
+	 */
 	private HashMap<String, Integer> map = new HashMap<String, Integer>();
+	/**
+	 * The total number of machine and the actual number of finisshed one
+	 */
 	private int nb_machines_tot;
 	private int nb_machine = 0;
+	/**
+	 * Variable for time analysis
+	 */
 	private String start;
 	private long strt;
 	private String middle;
 	private long middl;
+	/**
+	 * Arraylist of the results of the threads
+	 */
 	private ArrayList<Map> listMap;
-	private ArrayList<HashMap<String, Integer>> listHashMap;
+	// Name of the file
 	private String fileName;
 	private Logger log;
-	
+
+	/**
+	 * Constructor for the reducer
+	 * 
+	 * @param nb_machines The number of thread created
+	 * @param fileName    Name of the file
+	 * @param start       Time of beggining of the map as a string
+	 * @param strt        Time of beggining of the map in millis as a long
+	 */
 	public Reduce(int nb_machines, String fileName, String start, long strt) {
 		this.nb_machines_tot = nb_machines;
 		this.start = start;
 		this.strt = strt;
-		middle = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(Calendar.getInstance().getTime());
-		middl = Calendar.getInstance().getTimeInMillis();
+		this.middle = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(Calendar.getInstance().getTime());
+		this.middl = Calendar.getInstance().getTimeInMillis();
 		this.listMap = new ArrayList<Map>();
 		this.fileName = fileName;
 		this.log = Logger.getInstance();
 	}
 
-	public void addMap(Map m) {
+	/**
+	 * Function to give the result of a thread to the reducer
+	 * 
+	 * @param map
+	 */
+	public void addMap(Map map) {
 		nb_machine++;
-		this.listMap.add(m);
+		System.out.println("Recu 1");
+		this.listMap.add(map);
+
+		// If we received as many maps as they are threads, start calculating
 		if (nb_machine == nb_machines_tot) {
-			this.getMaps();
+			this.concatenateMaps();
 		}
 	}
-	
-	private void getMaps() {
+
+	/**
+	 * Function that launch the calculation and display the result
+	 */
+	private void concatenateMaps() {
 		for (Map m : this.listMap) {
 			this.add(m.getMapping());
 		}
-		long processingTime = Calendar.getInstance().getTimeInMillis()-strt;
-		long processingTimeMapReduce = Calendar.getInstance().getTimeInMillis()-middl;
+		long processingTime = Calendar.getInstance().getTimeInMillis() - strt;
+		long processingTimeMapReduce = Calendar.getInstance().getTimeInMillis() - middl;
 		String timeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(Calendar.getInstance().getTime());
 		try {
 			this.writeAndDisplayMap();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 		this.log.write("Reading start : " + start);
 		this.log.write("Map start : " + middle);
 		this.log.write("End   : " + timeStamp);
 		this.log.write("Processing time with reading: " + processingTime + " ms");
 		this.log.write("Processing time without reading: " + processingTimeMapReduce + " ms");
-	}
-	
-	public void add(HashMap<String, Integer> mapping) {
 		
+		String message = "Reading start : " + start + "\nMap start : " + middle + "\nEnd   : " + timeStamp + "\nProcessing time with reading: " + processingTime + " ms" + "\nProcessing time without reading: " + processingTimeMapReduce + " ms";
+
+JOptionPane.showMessageDialog(new JFrame(), message, "Resulat", JOptionPane.INFORMATION_MESSAGE);
+		
+		
+	}
+
+	/**
+	 * Function that combine the maps together
+	 * 
+	 * @param mapping A map of data from a thread
+	 */
+	public void add(HashMap<String, Integer> mapping) {
+
+		// For each word, if it's not in the map, add it, else add its value together
 		mapping.forEach((k, v) -> {
 			if (map.get(k) == null) {
 				map.put(k, v);
+			} else {
+				map.put(k, map.get(k) + v);
 			}
-			else {
-				map.put(k, map.get(k)+v);
-			}
-        });
+		});
 	}
-	
+
+	/**
+	 * Function to display in the console and write to a file
+	 * 
+	 * @throws IOException
+	 */
 	public void writeAndDisplayMap() throws IOException {
+
+		// Open a file with the same name and "_res"
+		FileWriter myWriter = new FileWriter(this.fileName + "_res.txt");
 		
-	
-		FileWriter myWriter = new FileWriter(this.fileName+"_res.txt");
-		
+		// For each word in the Hashmap, write it in the file and log it in the console
 		this.map.forEach((k, v) -> {
 			String str = v + " : " + k;
 			try {
-				myWriter.write(str+ "\n");
+				myWriter.write(str + "\n");
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 			this.log.write(str);
 		});
+		
+		myWriter.close();
 
 	}
 
 	@Override
 	public void run() {
-		
-		
+
 	}
 
 }
